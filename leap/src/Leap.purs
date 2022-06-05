a72a3dbe3a60c670dd27e5be7a8634c893f1caad
Ilya Shmygol <ishmygol@gmail.com>
2022-06-05 10:31:33 +0000

    Download exercise leap

---
+++ leap/src/Leap.purs
@@ -0,0 +1,8 @@
+module Leap
+  ( isLeapYear
+  ) where
+
+import Effect.Exception.Unsafe (unsafeThrow)
+
+isLeapYear :: Int -> Boolean
+isLeapYear = unsafeThrow "You need to implement this function."
