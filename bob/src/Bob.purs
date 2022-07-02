907ee19a4a71c9db2feb1f3da8c3686977eb3576
Ilya Shmygol <ishmygol@gmail.com>
2022-07-02 19:12:55 +0000

    Solve exercise bob v3

--- bob/src/Bob.purs
+++ bob/src/Bob.purs
@@ -3,40 +3,37 @@ module Bob
   ) where
 
 import Prelude
+import Data.Maybe
 import Data.String.CodeUnits (toCharArray) as String
 import Data.String.Common (trim) as String
 import Data.Enum (enumFromTo)
-import Data.Array.NonEmpty (NonEmptyArray, intersect, last, fromArray)
-import Data.Maybe (maybe)
+import Data.Array.NonEmpty (NonEmptyArray, intersect, last, fromArray) as NonEmptyA
 import Data.Array (null)
 
 hey :: String -> String
-hey = maybeSilance
-          "Fine. Be that way!"
-          case _ of
-              sentenceChars | isYelledQuestion sentenceChars -> "Calm down, I know what I'm doing!"
-                            | isYelling sentenceChars -> "Whoa, chill out!"
-                            | isQuestion sentenceChars -> "Sure."
-                            | otherwise -> "Whatever."
+hey = case _ of
+          Nothing -> "Fine. Be that way!"
+          Just(sentenceChars) | isYelledQuestion sentenceChars -> "Calm down, I know what I'm doing!"
+                              | isYelling sentenceChars -> "Whoa, chill out!"
+                              | isQuestion sentenceChars -> "Sure."
+                              | otherwise -> "Whatever."
+      <<< NonEmptyA.fromArray
       <<< String.toCharArray
       <<< String.trim
     where
-        maybeSilance :: String -> (NonEmptyArray Char -> String) -> Array Char -> String
-        maybeSilance answerToSilance f = maybe answerToSilance f <<< fromArray
-
-        lowerChars :: NonEmptyArray Char
+        lowerChars :: NonEmptyA.NonEmptyArray Char
         lowerChars = enumFromTo 'a' 'z'
 
-        upperChars :: NonEmptyArray Char
+        upperChars :: NonEmptyA.NonEmptyArray Char
         upperChars = enumFromTo 'A' 'Z'
 
-        isYelling :: NonEmptyArray Char -> Boolean
+        isYelling :: NonEmptyA.NonEmptyArray Char -> Boolean
         isYelling inputChars =
-            (null $ intersect inputChars lowerChars)
-            && (not $ null $ intersect inputChars upperChars)
+            (null $ NonEmptyA.intersect inputChars lowerChars)
+            && (not $ null $ NonEmptyA.intersect inputChars upperChars)
 
-        isQuestion :: NonEmptyArray Char -> Boolean
-        isQuestion inputChars = last inputChars == '?'
+        isQuestion :: NonEmptyA.NonEmptyArray Char -> Boolean
+        isQuestion inputChars = NonEmptyA.last inputChars == '?'
 
-        isYelledQuestion :: NonEmptyArray Char -> Boolean
+        isYelledQuestion :: NonEmptyA.NonEmptyArray Char -> Boolean
         isYelledQuestion inputChars = (isYelling inputChars) && (isQuestion inputChars)
