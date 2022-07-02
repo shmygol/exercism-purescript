f7472add1dd45bf59130f4702768ed440869c6ea
Ilya Shmygol <ishmygol@gmail.com>
2022-07-02 13:18:43 +0000

    Solve exercise bob v2

--- bob/src/Bob.purs
+++ bob/src/Bob.purs
@@ -6,18 +6,18 @@ import Prelude
 import Data.String.CodeUnits (toCharArray) as String
 import Data.String.Common (trim) as String
 import Data.Enum (enumFromTo)
-import Data.Array.NonEmpty (NonEmptyArray, intersect, last, fromArray, concat)
+import Data.Array.NonEmpty (NonEmptyArray, intersect, last, fromArray)
 import Data.Maybe (maybe)
 import Data.Array (null)
 
 hey :: String -> String
 hey = maybeSilance
-        "Fine. Be that way!"
-        case _ of
-            sentenceChars | isNormalQuestion sentenceChars -> "Sure."
-                          | isYelledQuestion sentenceChars -> "Calm down, I know what I'm doing!"
-                          | isYelling sentenceChars -> "Whoa, chill out!"
-                          | otherwise -> "Whatever."
+          "Fine. Be that way!"
+          case _ of
+              sentenceChars | isYelledQuestion sentenceChars -> "Calm down, I know what I'm doing!"
+                            | isYelling sentenceChars -> "Whoa, chill out!"
+                            | isQuestion sentenceChars -> "Sure."
+                            | otherwise -> "Whatever."
       <<< String.toCharArray
       <<< String.trim
     where
@@ -30,25 +30,13 @@ hey = maybeSilance
         upperChars :: NonEmptyArray Char
         upperChars = enumFromTo 'A' 'Z'
 
-        hasLowerChars :: NonEmptyArray Char -> Boolean
-        hasLowerChars inputChars = not $ null $ intersect inputChars lowerChars
-
-        hasUpperChars :: NonEmptyArray Char -> Boolean
-        hasUpperChars inputChars = not $ null $ intersect inputChars upperChars
-
         isYelling :: NonEmptyArray Char -> Boolean
-        isYelling inputChars = (not $ hasLowerChars inputChars) && (hasUpperChars inputChars)
+        isYelling inputChars =
+            (null $ intersect inputChars lowerChars)
+            && (not $ null $ intersect inputChars upperChars)
 
         isQuestion :: NonEmptyArray Char -> Boolean
         isQuestion inputChars = last inputChars == '?'
-
-        isNormalQuestion :: NonEmptyArray Char -> Boolean
-        isNormalQuestion inputChars =
-            (
-                (hasLowerChars inputChars)
-                || (not $ hasUpperChars inputChars)
-            )
-            && (isQuestion inputChars)
 
         isYelledQuestion :: NonEmptyArray Char -> Boolean
         isYelledQuestion inputChars = (isYelling inputChars) && (isQuestion inputChars)
