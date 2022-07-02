55934e3e501e7c05d297ef6dec75e7ec7092a9ba
Ilya Shmygol <ishmygol@gmail.com>
2022-07-02 16:44:30 +0000

    Solve exercise accumulate v1

--- accumulate/src/Accumulate.purs
+++ accumulate/src/Accumulate.purs
@@ -2,8 +2,8 @@ module Accumulate
   ( accumulate
   ) where
 
+import Prelude
 import Data.List (List)
-import Effect.Exception.Unsafe (unsafeThrow)
 
 accumulate :: forall a b. (a -> b) -> List a -> List b
-accumulate = unsafeThrow "You need to implement this function."
+accumulate = map
