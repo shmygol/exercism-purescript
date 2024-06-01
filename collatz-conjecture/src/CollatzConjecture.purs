cbfb884b47f56d60289718d880363ca19d4165c7
Ilya Shmygol <ishmygol@gmail.com>
2024-06-01 11:40:19 +0000

    Solve exercise collatz-conjecture v3

--- collatz-conjecture/src/CollatzConjecture.purs
+++ collatz-conjecture/src/CollatzConjecture.purs
@@ -4,14 +4,23 @@ module CollatzConjecture
 
 import Prelude
 import Data.Maybe (Maybe(Just, Nothing))
+import Data.Either (Either(Left, Right), hush)
 import Data.Int (even)
 
 collatz :: Int -> Maybe Int
-collatz = recursive_collatz 0
+collatz = hush <<< verbose_collatz 
+
+newtype Error = Error String
+
+error :: forall a. String -> Either Error a
+error = Left <<< Error
+
+verbose_collatz :: Int -> Either Error Int
+verbose_collatz = recursive_collatz 0
   where
     recursive_collatz counter x
-      | x < 1 = Nothing
-      | x == 1 = Just counter
+      | x < 1 = error "x must be greater than 0"
+      | x == 1 = Right counter
       | otherwise =
         recursive_collatz 
           (counter + 1)
