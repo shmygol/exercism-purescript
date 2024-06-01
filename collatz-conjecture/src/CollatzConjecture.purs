7ae369140a212adedeffb7fd7794281263f1aa67
Ilya Shmygol <ishmygol@gmail.com>
2024-06-01 09:31:00 +0000

    Solve exercise collatz-conjecture v1

--- collatz-conjecture/src/CollatzConjecture.purs
+++ collatz-conjecture/src/CollatzConjecture.purs
@@ -2,8 +2,19 @@ module CollatzConjecture
   ( collatz
   ) where
 
-import Data.Maybe (Maybe)
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Int (even)
 
 collatz :: Int -> Maybe Int
-collatz = unsafeThrow "You need to implement this function."
+collatz = recursive_collatz (Just 0)
+  where
+    incr = map $ add 1
+    recursive_collatz maybe_counter x
+      | x < 1 = Nothing
+      | x == 1 = maybe_counter
+      | otherwise =
+        recursive_collatz 
+          (incr maybe_counter)
+          if even x then (x / 2) else (x * 3 + 1)
+  
