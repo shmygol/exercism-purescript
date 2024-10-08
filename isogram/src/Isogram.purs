fddfa4552343057266c259428c82706ddd1fff88
Ilya Shmygol <ishmygol@gmail.com>
2024-10-08 20:41:46 +0200

    Download exercise isogram

---
+++ isogram/src/Isogram.purs
@@ -0,0 +1,8 @@
+module Isogram
+  ( isIsogram
+  ) where
+
+import Effect.Exception.Unsafe (unsafeThrow)
+
+isIsogram :: String -> Boolean
+isIsogram = unsafeThrow "You need to implement this function."
