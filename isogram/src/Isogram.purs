d5459c9cb83a15aa0a9430330464b816173709bf
Ilya Shmygol <ishmygol@gmail.com>
2024-10-11 20:53:11 +0200

    Solve exercise isogram v1

--- isogram/src/Isogram.purs
+++ isogram/src/Isogram.purs
@@ -2,7 +2,25 @@ module Isogram
   ( isIsogram
   ) where
 
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.Enum (enumFromTo)
+import Data.List as List
+import Data.List ((:))
+import Data.Maybe
+import Data.String.CodeUnits (toCharArray)
+import Data.String.Common (toLower)
+import Data.Set as Set
 
 isIsogram :: String -> Boolean
-isIsogram = unsafeThrow "You need to implement this function."
+isIsogram = isIsogram' Set.empty
+            <<< List.fromFoldable
+            <<< toCharArray
+            <<< toLower
+    where
+        alphabet = Set.fromFoldable (enumFromTo 'a' 'z' :: Array Char)
+        isIsogram' :: Set.Set Char -> List.List Char -> Boolean
+        isIsogram' visited List.Nil = true
+        isIsogram' visited (cur:tail)
+            | not $ Set.member cur alphabet = isIsogram' visited tail
+            | not $ Set.member cur visited = isIsogram' (Set.insert cur visited) tail
+            | otherwise = false
