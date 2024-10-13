da1dcddb9aa8ac961a8a8d409300366edde115c8
Ilya Shmygol <ishmygol@gmail.com>
2024-10-13 12:58:41 +0200

    Solve exercise rna-transcription v1

--- rna-transcription/src/RnaTranscription.purs
+++ rna-transcription/src/RnaTranscription.purs
@@ -2,8 +2,27 @@ module RnaTranscription
   ( toRNA
   ) where
 
-import Data.Maybe (Maybe)
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.Maybe
+import Data.Array (foldl)
+import Data.Map as Map
+import Data.String.CodeUnits as String
+import Data.Tuple (Tuple(..))
+
+rules :: Map.Map Char Char
+rules = Map.fromFoldable [(Tuple 'G' 'C'),
+                          (Tuple 'C' 'G'),
+                          (Tuple 'T' 'A'),
+                          (Tuple 'A' 'U')]
+
+transcript :: Char -> Maybe Char
+transcript = Map.lookup <@> rules
 
 toRNA :: String -> Maybe String
-toRNA = unsafeThrow "You need to implement this function."
+toRNA = foldl foldingFunction (Just "")
+        <<< String.toCharArray
+    where
+        foldingFunction :: Maybe String -> Char -> Maybe String
+        foldingFunction acc a = sonc <$> (transcript a) <*> acc
+        sonc :: Char -> String -> String
+        sonc c s = s <> String.singleton c
