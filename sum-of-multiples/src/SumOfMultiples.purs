51696b40e935c9ebca5bf1fba671cce1ae5010a1
Ilya Shmygol <ishmygol@gmail.com>
2024-10-14 22:38:55 +0200

    Solve exercise sum-of-multiples v1

--- sum-of-multiples/src/SumOfMultiples.purs
+++ sum-of-multiples/src/SumOfMultiples.purs
@@ -2,7 +2,18 @@ module SumOfMultiples
   ( sumOfMultiples
   ) where
 
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.Foldable (sum)
+import Data.Set as Set
+import Data.List.Lazy as List
+
+energyMultiplies :: Int -> Int -> Set.Set Int
+energyMultiplies level item = Set.fromFoldable
+                              $ List.takeWhile (level > _)
+                              $ List.iterate (item + _) item
 
 sumOfMultiples :: Array Int -> Int -> Int
-sumOfMultiples = unsafeThrow "You need to implement this function."
+sumOfMultiples items level = sum
+                             $ Set.unions
+                             $ Set.map (energyMultiplies level)
+                             $ Set.fromFoldable items
