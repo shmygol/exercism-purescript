2153eb6173723aaaf8a881040a12a249e8f58314
Ilya Shmygol <ishmygol@gmail.com>
2024-10-19 15:47:38 +0200

    Solve exercise atbash-cipher v1

--- atbash-cipher/src/AtbashCipher.purs
+++ atbash-cipher/src/AtbashCipher.purs
@@ -3,11 +3,49 @@ module AtbashCipher
   , encode
   ) where
 
-import Data.Maybe (Maybe)
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.Maybe
+import Data.Enum (enumFromTo)
+import Data.String.Common as String
+import Data.String.CodeUnits as String
+import Data.Array (zip)
+import Data.Foldable (and)
+import Data.Map as Map
+
+vocabularies :: { encode :: Map.Map Char Char, decode :: Map.Map Char Char }
+vocabularies = let enumAsc = enumFromTo 'a' 'z'
+                   enumDesc =  enumFromTo 'z' 'a' in
+               {
+                 encode: Map.fromFoldable (zip enumAsc enumDesc),
+                 decode: Map.fromFoldable (zip enumDesc enumAsc)
+               }
+
+isDigit :: Char -> Boolean
+isDigit = and <<< flap [(_ >= '0'), (_ <= '9')]
+
+mapChar :: Map.Map Char Char -> Char -> String
+mapChar vocab c | isDigit c = String.singleton c
+                | otherwise = maybe "" String.singleton $ Map.lookup c vocab
+
+mapStringWith :: Map.Map Char Char -> (String -> Int -> String) -> String -> String
+mapStringWith vocab f = String.trim
+                        <<< mapStringWith' 0 ""
+                        <<< String.uncons
+                        <<< String.toLower
+    where
+        mapStringWith' :: Int -> String -> Maybe { head :: Char, tail :: String } -> String
+        mapStringWith' _ acc Nothing = acc
+        mapStringWith' i acc (Just { head, tail }) =
+          case mapChar vocab head of
+            "" -> mapStringWith' i acc (String.uncons tail)
+            mappedString -> mapStringWith' (i + 1) (append acc (f mappedString i)) (String.uncons tail) 
 
 decode :: String -> Maybe String
-decode = unsafeThrow "You need to implement `decode`."
+decode = Just <<< mapStringWith vocabularies.decode const
 
 encode :: String -> Maybe String
-encode = unsafeThrow "You need to implement `encode`."
+encode = Just <<< mapStringWith vocabularies.encode prependSeparator
+    where
+        prependSeparator :: String -> Int -> String
+        prependSeparator s i | i `mod` 5 == 0 = " " <> s
+                             | otherwise = s
