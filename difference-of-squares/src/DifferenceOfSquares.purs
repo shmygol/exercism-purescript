0a60172cca7efc3ee6e78b00136b8c94bdaa1e69
Ilya Shmygol <ishmygol@gmail.com>
2024-10-29 23:13:10 +0100

    Download exercise difference-of-squares

---
+++ difference-of-squares/src/DifferenceOfSquares.purs
@@ -0,0 +1,16 @@
+module DifferenceOfSquares
+  ( differenceOfSquares
+  , squareOfSum
+  , sumOfSquares
+  ) where
+
+import Effect.Exception.Unsafe (unsafeThrow)
+
+differenceOfSquares :: Int -> Int
+differenceOfSquares = unsafeThrow "You need to implement `differenceOfSquares`."
+
+squareOfSum :: Int -> Int
+squareOfSum = unsafeThrow "You need to implement `squareOfSum`."
+
+sumOfSquares :: Int -> Int
+sumOfSquares = unsafeThrow "You need to implement `sumOfSquares`."
