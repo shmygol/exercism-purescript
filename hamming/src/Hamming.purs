8bdc7975846d40bc825b752d8386cfe8164b4b2c
Ilya Shmygol <ishmygol@gmail.com>
2024-10-30 12:57:58 +0100

    Solve exercise hamming v1

--- hamming/src/Hamming.purs
+++ hamming/src/Hamming.purs
@@ -2,8 +2,19 @@ module Hamming
   ( distance
   ) where
 
-import Data.Maybe (Maybe)
-import Effect.Exception.Unsafe (unsafeThrow)
+import Prelude
+import Data.Maybe
+import Data.String (length)
+import Data.String.CodeUnits (toCharArray)
+import Data.Array (zipWith)
+import Data.Foldable (sum)
 
 distance :: String -> String -> Maybe Int
-distance = unsafeThrow "You need to implement this function."
+distance a b | length a /= length b = Nothing
+             | otherwise = Just
+                           $ sum
+                           $ zipWith distanceForChars (toCharArray a) (toCharArray b)
+  where
+    distanceForChars :: Char -> Char -> Int
+    distanceForChars x y | x == y = 0
+                         | otherwise = 1
