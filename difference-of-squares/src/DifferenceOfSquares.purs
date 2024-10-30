14060cd811da4b63e8525722a5b0e299bdb3ba0c
Ilya Shmygol <ishmygol@gmail.com>
2024-10-30 11:24:56 +0100

    Solve exercise difference-of-squares v2

--- difference-of-squares/src/DifferenceOfSquares.purs
+++ difference-of-squares/src/DifferenceOfSquares.purs
@@ -6,18 +6,21 @@ module DifferenceOfSquares
 
 
 import Prelude
-import Data.Foldable (sum)
-import Data.Array (range)
-import Data.Int (pow)
-
-square :: Int -> Int
-square = flip pow 2
+import Math (pow)
+import Data.Int (toNumber, floor)
 
 differenceOfSquares :: Int -> Int
 differenceOfSquares n = (squareOfSum n) - (sumOfSquares n)
 
 squareOfSum :: Int -> Int
-squareOfSum = square <<< sum <<< range 1
+squareOfSum = floor <<< squareOfSumNumber <<< toNumber
+  where
+    squareOfSumNumber n = pow <@> 2.0
+                          $ (n / 2.0) * (n + 1.0)
 
 sumOfSquares :: Int -> Int
-sumOfSquares = sum <<< map square <<< range 1
+sumOfSquares = floor <<< sumOfSquaresNumber <<< toNumber
+  where
+    sumOfSquaresNumber n = (pow n 3.0) / 3.0
+                           + (pow n 2.0) / 2.0
+                           + n / 6.0
